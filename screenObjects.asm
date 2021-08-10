2ab872312b8bf56114ca43ed5e30d4d28d48e627
Zarecki <jonzarecki@gmail.com>
2021-08-10 15:26:26 +0100

    initial commit

---
+++ screenObjects.asm
@@ -0,0 +1,210 @@
+    processor 6502
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Include reuired files                                                                           ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    include "vcs.h"
+    include "macro.h"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start an uninitialised segment at $80 for variable declaration                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    seg.u Variables
+    org $80
+
+Player0Height ds 1      ;Defines one space for player 0 height
+Player1Height ds 1
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start the ROM code segment                                                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    seg
+    org $F000
+
+Reset:
+    CLEAN_START         ;Macro to clean memory and TIA
+
+    ldx #$80            ;Blue background
+    stx COLUBK
+
+    lda #%1111          ;White playfield
+    sta COLUPF
+
+    lda #10             ;A=10       
+    sta Player0Height   ;Player0Height = 10
+    sta Player1Height   ;Player1Height = 10
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set the TIA registers for Player 0 and Player 1 colours                                        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    lda #$48        ;P0 colour light red
+    sta COLUP0
+
+    lda #$C6        ;P1 colour light green
+    sta COLUP1
+
+    ldy #%00000010  ;CTRLPF D1 set to 1 sets "score" flag - causes scores to reflect the player's colour
+    sty CTRLPF
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start a new frame - configure VSYNC and VBLANK                                                  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+StartFrame:
+    lda #2
+    sta VBLANK      ;Turn VBLANK on
+    sta VSYNC       ;Turn VSYNC on
+
+    REPEAT 3
+        sta WSYNC   ;First three VYSNC scanlines
+    REPEND
+
+    lda #0
+    sta VSYNC       ;Turn VSYNC off
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 37 lines of VBLANK                                                                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    REPEAT 37
+        sta WSYNC
+    REPEND
+
+    sta VBLANK      ;Turn VBLANK off
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 192 visible scanlines                                                                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+VisibleScanlines:
+    ;10 emply lines at the top
+    REPEAT 10
+        sta WSYNC
+    REPEND
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 10 lines for the scoreboard                                                                    ;;
+;; Pulls data from an array of bytes defined at NumberBitmap                                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ldy #0
+ScoreboardLoop:
+    lda NumberBitmap,Y
+    sta PF1
+    sta WSYNC
+    iny
+    cpy #10
+    bne ScoreboardLoop
+
+    lda #0
+    sta PF1         ;Disable playfield
+
+    ;Draw 50 empty lines between scoreboard and player
+    REPEAT 50
+        sta WSYNC
+    REPEND
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 10 lines for the Player 0 graphics                                                             ;;
+;; Pulls data from an array of bytes defined at PlayerBitmap                                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ldy #0
+Player0Loop:
+    lda PlayerBitmap,Y
+    sta GRP0
+    sta WSYNC
+    iny
+    cpy Player0Height
+    bne Player0Loop
+
+    lda #0
+    sta GRP0        ;Disable player 0 graphics
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 10 lines for the player 1 graphics                                                             ;;
+;; Pulls data from an array of bytes defined at PlayerBitmap                                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ldy #0
+Player1Loop:
+    lda PlayerBitmap,Y
+    sta GRP1
+    sta WSYNC
+    iny
+    cpy Player1Height
+    bne Player1Loop
+
+    lda #0
+    sta GRP1        ;Disable player 1 graphics
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; remaining 102 scanlines (192-90)                                                               ;;
+;; already used 10+10+50+10+10 = 90                                                               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    REPEAT 102
+        sta WSYNC
+    REPEND
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 30 lines for VBLANK overscan                                                                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    REPEAT 30
+        sta WSYNC
+    REPEND
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Loop to next frame                                                                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    jmp StartFrame
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Defines an array of bytes to draw the player sprite                                             ;;
+;; We add these in the end ROM addresses                                                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    org $FFE8
+PlayerBitmap:
+    .byte #%01111110    ;  ###### 
+    .byte #%11111111    ; ########
+    .byte #%10011001    ; #  ##  #
+    .byte #%11111111    ; ########
+    .byte #%11111111    ; ########
+    .byte #%11111111    ; ########
+    .byte #%10111101    ; # #### #
+    .byte #%11000011    ; ##    ##
+    .byte #%11111111    ; ########
+    .byte #%01111110    ;  ###### 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Defines an array of bytes to draw the scoreboard number                                         ;;
+;; We add these in the penultimate ROM addresses                                                  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    org $FFF2
+NumberBitmap:
+    .byte #%00001110    ;     ### 
+    .byte #%00001110    ;     ### 
+    .byte #%00000010    ;       # 
+    .byte #%00000010    ;       #
+    .byte #%00001110    ;     ### 
+    .byte #%00001110    ;     ### 
+    .byte #%00001000    ;     #   
+    .byte #%00001000    ;     #   
+    .byte #%00001110    ;     ###   
+    .byte #%00001110    ;     ###
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Complete ROM size                                                                              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    org $FFFC
+    .word Reset
+    .word Reset
